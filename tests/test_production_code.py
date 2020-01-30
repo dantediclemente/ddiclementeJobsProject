fd2062b61bfa582c7a1d7c5cda70f90e1e0e36dd
Dante DiClemente <diclementedante@gmail.com>
2020-01-30 14:37:22 -0500

    Test files

---
+++ tests/test_production_code.py
@@ -0,0 +1,14 @@
+import pytest
+import requests
+from production_code import get_api_data
+import os.path
+
+
+def test_number_of_jobs():
+    jobs = get_api_data()
+    assert len(jobs) > 100
+
+
+def test_first_job_listing():
+    file = open(os.path.dirname(__file__) + '/../jobs.txt', 'r')
+    assert file.read(43) == '{\n    "company": "F. Hoffmann-La Roche AG",'
