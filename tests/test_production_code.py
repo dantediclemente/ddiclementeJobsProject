d0dd8f7d1c02adbaece0038904480cb881f83929
Dante DiClemente <diclementedante@gmail.com>
2020-01-30 15:31:56 -0500

    print

--- tests/test_production_code.py
+++ tests/test_production_code.py
@@ -8,5 +8,9 @@ def test_number_of_jobs():
 
 
 def test_first_job_listing():
-    file = open(os.path.dirname(__file__) + '/../jobs.txt', 'r')
-    assert file.read(43) == '{\n    "company": "F. Hoffmann-La Roche AG",'
+    job_file = open(os.path.dirname(__file__) + '/../jobs.txt', 'r')
+    name_found_in_line = False
+    for line in job_file:
+        if 'F. Hoffmann-La Roche AG' in line:
+            name_found_in_line = True
+    assert name_found_in_line
