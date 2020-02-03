d727275fe17208994c1e7bcd0c02af0ca3f8c74f
Dante DiClemente <diclementedante@gmail.com>
2020-02-03 16:26:08 -0500

    minor update

--- production_code.py
+++ production_code.py
@@ -5,19 +5,18 @@ import json
 def get_api_data():
     job_file = open('jobs.txt', 'w+')
     job_list = []
-    response_length = 1
     page_number = 1
 
-    while response_length != 0:
+    while page_number < 6:
         response = requests.get("https://jobs.github.com/positions.json?&page=" + str(page_number))
-        page_number += 1
-        response_length = len(response.json())
-        jobs = response.json()
-        for job in jobs:
-            job_list.append(json.dumps(job, sort_keys=True, indent=4))
+        print(response.status_code)
+        if response.status_code == 200:
+            page_number += 1
+            jobs = response.json()
+            for job in jobs:
+                job_list.append(json.dumps(job, sort_keys=True, indent=4))
     for job in job_list:
         job_file.write(job)
-        print(job)
     return job_list
 
 
